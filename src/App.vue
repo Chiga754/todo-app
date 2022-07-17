443faafd50bea07f27ddfabc63f597a2d78baf25
Chiga754 <a.chigarev96@gmail.com>
2022-07-17 13:28:48 +0500

    Added a component <TopMenu.vue>

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,43 @@
 <template>
-  <h1>Todo app</h1>
+  <div class="todo__main">
+    <top-menu></top-menu>
+  </div>
 </template>
 
+<script>
+import TopMenu from './components/TopMenu.vue';
+  export default {
+    components: {
+      TopMenu,
+    }
+  }
+</script>
+
 <style>
+*{
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+}
+body {
+  background: rgb(0, 204, 255);
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
+  display: flex;
+  justify-content: center;
+}
+.todo__main{
+  margin-top: 20px;
+  width: 1000px;
+  height: 700px;
+  background: #2a3744;
+  border-radius: 10px;
+  display: flex;
+  justify-content: space-between;
+  padding: 40px
 }
 </style>
---
+++ src/components/TopMenu.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="secton__date">
+        <div>Saturday, June 1</div>
+        <div class="active-tasks">3 Active Task</div> 
+    </div>
+
+    <div class="">
+        <a href="#" class="">Incomplete Tasks</a>
+        <a href="#">Complete Tasks</a>
+    </div>
+
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+    .secton__date{
+        color: #ffffff;
+        font-size: 20px;
+    }
+    a{
+        color: #d1d1d1d5;
+        text-decoration: none;
+        margin-left: 70px;
+        font-size: 18px;
+        transition: all .5s;
+    }
+    a:hover{
+        color: #ffffff;
+    }
+    .active{
+        color: #ffffff;
+    }   
+    .active-tasks{
+        font-size: 16px;
+        color: rgb(0, 204, 255);;
+    }
+</style>
